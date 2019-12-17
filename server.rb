0d5f08931adc650defc3d7b004e162e1d25aadda
Jay <jordanchu95@gmail.com>
2019-12-16 21:43:46 -0500

    todolist

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+# require "pry"
 
 set :bind, '0.0.0.0'  # bind to all interfaces
 set :public_folder, File.join(File.dirname(__FILE__), "public")
@@ -15,4 +16,19 @@ end
 get "/tasks/:task_name" do
   @task_name = params[:task_name]
   erb :show
+end
+
+post "/tasks" do
+  # binding.pry
+  # Read the input from the form the user filled out
+  task = params["task_name"]
+
+  # Open the "tasks.txt" file and append the task
+  File.open("tasks.txt", "a") do |file|
+    file.puts(task)
+  end
+
+  # Send the user back to the home page which shows
+  # the list of tasks
+  redirect "/tasks"
 end
